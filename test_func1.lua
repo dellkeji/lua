9d838251c2a3451d945d9709b615943eacbdb888
dellkeji <tzwydragon@163.com>
2017-01-07 15:36:18 +0800

    update

---
+++ test_func1.lua
@@ -0,0 +1,10 @@
+#!/usr/local/bin/lua
+-- test function one
+
+function myTest()
+    return 111
+end
+
+test = myTest()
+
+print(test)
---
+++ test_until.lua
@@ -0,0 +1,9 @@
+#!/usr/local/bin/lua
+-- test until
+
+sum = 2
+repeat
+    sum = sum ^ 2 --power or exponent
+until sum > 1000
+
+print(sum)
